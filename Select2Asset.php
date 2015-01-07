c3afb0e8ffabba39cd480cf3cf4d4a46385309a5
mbman <mario.baricevic@gmail.com>
2015-01-07 16:17:31 +0100

    Select2Asset rendering files when part of combined asset bugfix

--- Select2Asset.php
+++ Select2Asset.php
@@ -3,7 +3,7 @@
 /**
  * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014
  * @package yii2-widgets
- * @subpackage yii2-widget-select2 
+ * @subpackage yii2-widget-select2
  * @version 1.0.0
  */
 
@@ -23,6 +23,12 @@ class Select2Asset extends \kartik\base\AssetBundle
     public function init()
     {
         $this->setSourcePath(__DIR__ . '/lib');
+
+        // exit if using as part of combined asset
+        if (in_array('all', $this->depends)) {
+    		return;
+    	}
+
         $this->setupAssets('css', ['select2', 'select2-kv']);
         $this->setupAssets('js', ['select2', 'select2-kv']);
         parent::init();
