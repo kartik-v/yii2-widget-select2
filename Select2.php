1748cf9ef6eaa85ef5c3ea3019e0826c6613a83e
Delfi <delfi89@gmail.com>
2015-09-16 16:05:00 +0300

    Fix bug on embedAddon function

--- Select2.php
+++ Select2.php
@@ -174,7 +174,7 @@ class Select2 extends \kartik\base\InputWidget
      */
     protected function embedAddon($input)
     {
-        if (empty($this->addon)) {
+        if (!isset($this->size) && empty($this->addon)) {
             return $input;
         }
         $group = ArrayHelper::getValue($this->addon, 'groupOptions', []);
