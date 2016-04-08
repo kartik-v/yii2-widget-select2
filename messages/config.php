fe97869838bd11d781cfb3958fc4fca28d0385c7
Kanin Peanviriyakulkit <dogrocker@users.noreply.github.com>
2016-04-08 10:03:22 +0700

    Update config.php

--- messages/config.php
+++ messages/config.php
@@ -7,7 +7,7 @@ return [
     'messagePath' => __DIR__,
     // array, required, list of language codes that the extracted messages
     // should be translated to. For example, ['zh-CN', 'de'].
-    'languages' => ['ar', 'cs', 'da', 'de', 'el-GR', 'en', 'es', 'et', 'fa-IR', 'fr', 'hu', 'id', 'it', 'lt', 'nl', 'pl', 'pt', 'ru', 'sk', 'sv', 'tr', 'vi', 'zh'],
+    'languages' => ['ar', 'cs', 'da', 'de', 'el-GR', 'en', 'es', 'et', 'fa-IR', 'fr', 'hu', 'id', 'it', 'lt', 'nl', 'pl', 'pt', 'ru', 'sk', 'sv', 'th', 'tr', 'vi', 'zh'],
     // string, the name of the function for translating messages.
     // Defaults to 'Yii::t'. This is used as a mark to find the messages to be
     // translated. You may use a string for single function name or an array for
