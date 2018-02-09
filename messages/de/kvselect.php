ba8a749b8e274620c9703fc7f6581f0c450f8552
niki7har <niki7har@gmail.com>
2018-02-09 12:11:35 +0100

    Update kvselect.php

--- messages/de/kvselect.php
+++ messages/de/kvselect.php
@@ -17,6 +17,6 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-    'Select all' => '',
-    'Unselect all' => '',
+    'Select all' => 'Alle auswählen',
+    'Unselect all' => 'Auswahl aufheben',
 ];
