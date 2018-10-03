be12e5a4ed077f884d82feb58087ebc7bf60fca3
giannisdag <giannisdag@yahoo.gr>
2018-10-03 10:06:51 +0300

    Update kvselect.php

--- src/messages/el-GR/kvselect.php
+++ src/messages/el-GR/kvselect.php
@@ -17,6 +17,6 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-    'Select all' => '',
-    'Unselect all' => '',
+    'Select all' => 'Επιλογή όλων',
+    'Unselect all' => 'Αποεπιλογή όλων',
 ];
